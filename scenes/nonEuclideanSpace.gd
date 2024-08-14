bf28709dd597a1e16c5e0f30fc0442a609ca0c66
Jayden <everesja@gmail.com>
2024-08-14 21:56:14 +1200

    Added pillars to non Euclidean space

--- scenes/nonEuclideanSpace.gd
+++ scenes/nonEuclideanSpace.gd
@@ -9,9 +9,10 @@ func _ready():
 	$dimension3.position.y = -100
 	$dimension3.visible = false
 	$dimension4.visible = false
+	$pillarsLeft.visible = false
+	$pillarsRight.visible = false
 
 func _on_dimension_1_transition_boundary_screen_exited():
-	print("aaaa")
 	if nextDimensionQueued and dimensionNum == 1:
 		dimensionNum = 2
 		nextDimensionQueued = false
@@ -33,15 +34,21 @@ func _on_dimension_2_transition_boundary_screen_exited():
 func _on_first_story_entrance_field_body_entered(body):
 	story = 1
 	$dimension3.visible = false
+	$pillarsLeft.visible = false
 
 func _on_second_story_entrance_field_body_entered(body):
 	story = 2
 	$dimension3.visible = true
-
+	$pillarsLeft.visible = true
 
 func _on_third_story_entrance_field_body_entered(body):
 	$dimension4.visible = false
 
-
 func _on_forth_story_entrance_field_body_entered(body):
 	$dimension4.visible = true
+
+func _on_second_story_tunnel_entrance_field_body_entered(body):
+	$pillarsRight.visible = false
+
+func _on_second_story_exit_field_body_entered(body):
+	$pillarsRight.visible = true
